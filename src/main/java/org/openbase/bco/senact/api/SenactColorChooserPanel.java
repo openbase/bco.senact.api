a4daeace7e8379f8e95deb2f2686bdefd515acd3
Divine Threepwood <divine@openbase.org>
2024-01-24 14:07:37 +0100

    header updates

--- src/main/java/org/openbase/bco/senact/api/SenactColorChooserPanel.java
+++ src/main/java/org/openbase/bco/senact/api/SenactColorChooserPanel.java
@@ -4,7 +4,7 @@ package org.openbase.bco.senact.api;
  * #%L
  * BCO Senact API
  * %%
- * Copyright (C) 2013 - 2019 openbase.org
+ * Copyright (C) 2013 - 2024 openbase.org
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
